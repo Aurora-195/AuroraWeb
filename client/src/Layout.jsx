86048f1ceada479d9860a2323fc3ed1c8c99391b
Dave <davistran02@gmail.com>
2024-02-02 14:34:42 -0800

    Made changes to layout
    - dashboard tab only shows if you're logged in
    - log/register turns into logout if logged in

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
-import {Link} from "react-router-dom";
-import React, { useState } from 'react';
+import {Link, useLocation} from "react-router-dom";
+import React, { useEffect, useContext, useState } from 'react';
 
 export default function Layout() {
     const [isExpanded, setExpanded] = useState(true)
+
+    const location = useLocation();
+    const userData = location.state?.user.user;
+    const isLoggedIn = userData?.login;
 
     return (
         <div className="flex flex-col h-screen">
@@ -26,16 +30,23 @@ export default function Layout() {
             </div>
             <div className="flex-1 flex flex-row">
                 <nav className={`transition-all ${isExpanded ? "w-0 -ml-10" : "w-48 h-full"} flex flex-col bg-gradient-to-r from-purple-600 to-[#8A69D7] text-white font-bold`}>
-                    <Link to={'/main'}>
-                        <button className={`transition-all ${isExpanded ? "w-0 invisible" : "w-full"} text-left hover:bg-purple-500 p-4`}>
-                            <div className="flex flex-row space-x-2 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                                </svg>
-                                <div>Dashboard</div>
-                            </div>
-                        </button>
-                    </Link>
+                    {
+                        isLoggedIn
+                        ?
+                        <Link to={'/main'}>
+                            <button className={`transition-all ${isExpanded ? "w-0 invisible" : "w-full"} text-left hover:bg-purple-500 p-4`}>
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                                    </svg>
+                                    <div>Dashboard</div>
+                                </div>
+                            </button>
+                        </Link>
+                        :
+                        null
+                    }
+
                     
                     <Link to={'/'}>
                         <button className={`transition-all ${isExpanded ? "w-0 invisible" : "w-full"} text-left hover:bg-purple-500 p-4`}>
@@ -54,7 +65,12 @@ export default function Layout() {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
-                                <div>Login/Register</div>
+                                {
+                                    isLoggedIn ? 
+                                    <div>Logout</div> 
+                                    : 
+                                    <div>Login/Register</div> 
+                                }
                             </div>
                         </button>
                     </Link>
